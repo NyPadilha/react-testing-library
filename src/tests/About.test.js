59329b7324b9d2d09efac93cd018d28e6dd99531
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-05-31 16:56:30 -0300

    feat: App and About tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Component <About.js /> test', () => {
+  //   it('Teste se a página contém as informações sobre a Pokédex;', () => {
+  //   });
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const about = screen.getByRole('heading', { level: 2 });
+    expect(about).toHaveTextContent('About Pokédex');
+  });
+  //   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex;', () => {
+  //   });
+  it('Teste se a página contém a imagem da Pokédex', () => {
+    renderWithRouter(<About />);
+    const { src } = screen.getByRole('img');
+    expect(src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Component <App.js /> test', () => {
+  it('O primeiro link deve possuir o texto Home e estar na tela', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
+  it('O segundo link deve possuir o texto About e estar na tela', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+  });
+  it('O terceiro link deve possuir o texto Favorite Pokémon e estar na tela', () => {
+    renderWithRouter(<App />);
+    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(favorite).toBeInTheDocument();
+  });
+  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/not-found');
+    });
+    const notFound = screen.getByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
